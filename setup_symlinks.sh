fb7caa669952917e11619f6b0af542b9fd56c8a9
Jeff Dickey <dickeytk@gmail.com>
2011-05-15 17:42:11 -0700

    added global gitignore

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/.dotfiles/cvsignore ~/.cvsignore
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/rvmrc ~/.rvmrc
