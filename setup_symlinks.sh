efb9b521b4e38f004e81663079bc6fd6fefcc805
Jeff Dickey <dickeytk@gmail.com>
2011-05-21 15:45:23 -0700

    added gitcompletion

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -6,3 +6,4 @@ ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/rvmrc ~/.rvmrc
+ln -s ~/.dotfiles/git-completion.bash ~/.git-completion.bash
