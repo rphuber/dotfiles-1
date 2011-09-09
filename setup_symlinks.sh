39144888e5a847bf0dddf7c50ebc990255795614
Jeff Dickey <dickeytk@gmail.com>
2011-09-09 10:25:43 -0700

    Added gemrc

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -7,3 +7,4 @@ ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/rvmrc ~/.rvmrc
 ln -s ~/.dotfiles/git-completion.bash ~/.git-completion.bash
+ln -s ~/.dotfiles/gemrc ~/.gemrc
