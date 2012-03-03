3cb14eaae0be4e21b01937bb7855195e2d3b34bd
Jeff Dickey <me@jeffdickey.info>
2012-03-03 11:49:13 -0800

    fixed terminal

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -9,6 +9,8 @@ ln -sf ~/.dotfiles/rvmrc ~/.rvmrc
 ln -sf ~/.dotfiles/git-completion.bash ~/.git-completion.bash
 ln -sf ~/.dotfiles/gemrc ~/.gemrc
 ln -sf ~/.dotfiles/ackrc ~/.ackrc
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sfn ~/.dotfiles/dotjs ~/.js
 ln -sfn ~/.dotfiles/iterm2 ~/.iterm2
+ln -sfn ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
 source ~/.bash_profile
